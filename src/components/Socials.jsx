03aa949a1edad25faac430432937a899247179e0
AhmadMahmoudTarfaoui <ahmadmahmoudtarfaoui@outlook.com>
2024-11-16 09:26:22 +0100

    added github link

---
+++ src/components/Socials.jsx
@@ -0,0 +1,10 @@
+import { FaGithub } from "react-icons/fa";
+
+
+const Socials = () => {
+  return (
+    <a href="https://github.com/AhmedMahmoudT/tic-tac-toe" target="_blank" className="absolute top-10 right-10 text-4xl"><FaGithub /></a>
+  )
+}
+
+export default Socials
